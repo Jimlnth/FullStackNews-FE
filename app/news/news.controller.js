bb538814f158d9895af7ab17687a78e9f134ffe6
JimmyLanthin <lanthinjimmy@gmail.com>
2016-01-28 17:05:53 +0100

    Samir BG

--- app/news/news.controller.js
+++ app/news/news.controller.js
@@ -1,9 +1,41 @@
 (function() {
+	'use strict';
+
 	angular
 		.module('app.news')
-		.controller('NewsController', TestController);
+		.controller('NewsController', NewsController);
 
-	NewsController = function() {
+	NewsController.$inject = ['NewsService'];
 
+	function NewsController(NewsService) {
+		var vm = this;
+		NewsService.getNews().then(function(news) {
+		 	vm.news = news;
+		});
+
+		vm.vote = function(index) {
+			NewsService.updateVotes(vm.news[index]._id).then(function() {
+				vm.news[index].votes++;
+			}).catch(function(err) {
+				console.log(err);
+			});
+		}
+
+		vm.comment = function(index) {
+			if (! vm.textComment[index]) {
+				return;
+			}
+
+			NewsService.addComment(vm.news[index]._id, vm.textComment[index]).then(function(comment) {
+				vm.news[index].comments.push(comment);
+				vm.textComment[index] = "";
+			}).catch(function(err) {
+				console.log(err);
+			});
+		}
+
+		vm.create = function() {
+			
+		}
 	}
 })();
